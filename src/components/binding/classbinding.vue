2dc2760dde050d715554d3146cb4c1e9db7bc150
dharmikbhesaniya <117299507+dharmikbhesaniya@users.noreply.github.com>
2022-11-16 23:08:53 +0530

    binding
    binding

--- src/components/binding/classbinding.vue
+++ src/components/binding/classbinding.vue
@@ -1,9 +1,57 @@
 <template lang="">
-    <div>hello</div>
+    <div>
+        <h1 :class="apply">hello</h1>
+        <button v-on:click="chan()">to chang bgcolor</button>&nbsp;&nbsp;&nbsp;
+        <button v-on:click="chan1()">to chang textcolor</button
+        >&nbsp;&nbsp;&nbsp;
+        <button v-on:click="chan2()">to chang font size</button
+        >&nbsp;&nbsp;&nbsp;
+    </div>
 </template>
 <script>
 export default {
     name: "classbinding",
+    data() {
+        return {
+            bgcolo: true,
+            colo: true,
+            fon: true,
+        };
+    },
+    computed: {
+        apply() {
+            return {
+                h1: this.bgcolo,
+                red: this.colo,
+                fo: this.fon,
+            };
+        },
+    },
+    methods: {
+        chan() {
+            this.bgcolo = !this.bgcolo;
+        },
+        chan1() {
+            this.colo = !this.colo;
+        },
+        chan2() {
+            this.fon = !this.fon;
+        },
+    },
 };
 </script>
-<style lang=""></style>
+<style scoped>
+.h1 {
+    background-color: aqua;
+    padding: 10px;
+}
+h1 {
+    width: 200px;
+}
+.red {
+    color: red;
+}
+.fo {
+    font-size: 50px;
+}
+</style>
