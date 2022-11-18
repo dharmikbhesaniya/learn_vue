734d47f122ca5f93f63b67ecce8ef00c3c6588e4
dharmikbhesaniya <117299507+dharmikbhesaniya@users.noreply.github.com>
2022-11-18 11:05:39 +0530

    validetion
    validetion

--- src/components/binding/Valuegetfrominput.vue
+++ src/components/binding/Valuegetfrominput.vue
@@ -1,53 +1,34 @@
 <template lang="">
-    <div>
-        <!-- two way binding useing v-model -->
-        <input type="text" name="" id="" v-model="email" />
-        <br />
-        <br />
-        <input type="text" name="" id="" v-model="pass" />
-        <br /><br />
-        <h1>{{ email }}</h1>
-        <h1>{{ pass }}</h1>
-        <br /><br />
+<div>
+    <!-- two way binding useing v-model with velidetion -->
+    <input type="text" name="" id="" v-model="form.email" />
+    <br />
+    <br />
+    <input type="text" name="" id="" v-model="form.pass" />
+    <br /><br />
+    <h1>{{ form.email }}</h1>
+    <h1>{{ form.pass }}</h1>
+    <br /><br />
 
-        <input type="checkbox" name="" value="java" id="java" v-model="tech" />
-        <label for="java">java</label>
+    <input type="checkbox" name="" value="java" id="java" v-model="form.tech" />
+    <label for="java">java</label>
 
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <input
-            type="checkbox"
-            name=""
-            value="javascript"
-            id="javascript"
-            v-model="tech"
-        />
-        <label for="javascript">javascript</label>
-        <h1>{{ tech }}</h1>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="checkbox" name="" value="javascript" id="javascript" v-model="form.tech" />
+    <label for="javascript">javascript</label>
+    <h1>{{ form.tech }}</h1>
 
-        <br /><br /><br />
-        <input
-            type="radio"
-            name="fa"
-            value="devloper"
-            id="devloper"
-            v-model="info"
-        />
-        <label for="devloper">devloper</label>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <input
-            type="radio"
-            name="fa"
-            value="programar"
-            id="programar"
-            enable
-            v-model="info"
-        />
-        <label for="programar">programar</label>
-        <h1>{{ info }}</h1>
-        <br /><br />
-        <button type="button" v-on:click="getdata()">get value</button>
-        <br /><br />
-    </div>
+    <br /><br /><br />
+    <input type="radio" name="fa" value="devloper" id="devloper" v-model="form.info" />
+    <label for="devloper">devloper</label>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="radio" name="fa" value="programar" id="programar" enable v-model="form.info" />
+    <label for="programar">programar</label>
+    <h1>{{ form.info }}</h1>
+    <br /><br />
+    <button type="button" v-on:click="getdata()">get value</button>
+    <br /><br />
+</div>
 </template>
 
 <script>
@@ -56,19 +37,31 @@ export default {
     data() {
         return {
             form: {
-                email: null,
-                pass: null,
+                email: "",
+                pass: "",
                 tech: [],
                 info: "",
             },
+            error: [],
         };
     },
     methods: {
         getdata() {
-            console.log(`Email: ${this.email}`);
-            console.log(`pass: ${this.pass}`);
-            console.log(`tech: ${this.tech}`);
-            console.log(`info: ${this.info}`);
+            this.error = [];
+            for (let i in this.form) {
+                if (this.form[i] === "" || this.form[i].length === 0) {
+                    this.error.push(i);
+                }
+            }
+            if (this.error.length < 1) {
+                console.log(`Email: ${this.form.email}`);
+                console.log(`pass: ${this.form.pass}`);
+                console.log(`tech: ${this.form.tech}`);
+                console.log(`info: ${this.form.info}`);
+                alert("sucsess");
+            } else {
+                console.log(this.error);
+            }
         },
     },
 };
